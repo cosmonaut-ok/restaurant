a52d97b75c2b40a0f13cd7fabc2b7230ee301ee9
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2016-08-25 18:58:55 +0300

    Fix bootstrapping, pre-release version bump

--- src/restaurant-help.el
+++ src/restaurant-help.el
@@ -29,7 +29,7 @@
 
 (defun restaurant-version (&optional short)
   (interactive)
-  (let ((version "0.1.CURRENT")) ;; autoconf-anchor
+  (let ((version "0.2.0rc1")) ;; autoconf-anchor
     (if short
 	(princ version)
       (princ (concat "Restaurant Chef IDE version: " version ".\nBuild with Emacs version:\n" (emacs-version) "\n")))))
