0b632d3fe9f6fbcff5b837c85fea72de956c5c89
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2016-11-18 18:29:02 +0200

    Fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ else
     RUBY_VERSION=2.2
 fi
 
+SCRIPT_HOME="$(dirname `realpath $0`)"
+
 rvm_installed_p ()
 {
   which rvm 2>/dev/null
@@ -27,17 +29,17 @@ bootstrap_rvm ()
     curl -sSL https://rvm.io/mpapis.asc | gpg --import -
     # bootstrap RVM
     curl -sSL https://get.rvm.io | bash -s stable --ruby=${RUBY_VERSION} --auto-dotfiles --gems=bundler
-    . ${HOME}/.rvm/scripts/rvm
   fi
   # install required gems
+  . ${HOME}/.rvm/scripts/rvm
+  cd $SCRIPT_HOME
   bundle install
 }
 
 bootstrap_rvm
 
-CURRENT_PATH="$(dirname `realpath $0`)"
-FIX_PATH="$(echo $CURRENT_PATH | sed 's/\//\\\//g')"
+FIX_PATH="$(echo $SCRIPT_HOME | sed 's/\//\\\//g')"
 
-sed "s/\@HERE\@/$FIX_PATH/g" $CURRENT_PATH/data/restaurant.desktop.in > $CURRENT_PATH/Restaurant.desktop
+sed "s/\@HERE\@/$FIX_PATH/g" $SCRIPT_HOME/data/restaurant.desktop.in > $SCRIPT_HOME/Restaurant.desktop
 	
 printf "\n\nYou must run command 'source ${HOME}/.rvm/scripts/rvm' before launching restaurant, or just restart 'restaurant' if it already launched\n\n"
