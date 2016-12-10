dc0b678dcd834cdbaa5221d180091245e36c2001
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2016-12-11 00:10:55 +0200

    Some company extensions

--- src/restaurant-company.el
+++ src/restaurant-company.el
@@ -47,6 +47,8 @@
 
 (define-key company-active-map (kbd "C-n") #'company-select-next)
 (define-key company-active-map (kbd "C-p") #'company-select-previous)
+(define-key company-active-map (kbd "C-d") #'company-show-doc-buffer)
+(define-key company-active-map (kbd "M-.") #'company-show-location)
 
 (eval-after-load 'company
   (lambda ()
