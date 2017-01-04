cd43157930776d3cd724bc81264e43256cfad40b
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2017-01-04 19:41:35 +0200

    Move to only gtk3 version support

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 autoconf
-for i in gtk2 gtk3; do
-    ./configure --with-gui=${i}
-    make release
-    make clean-emacs
-done    
+./configure --with-gui=gtk3
+make release
+make clean-emacs
