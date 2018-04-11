9fc74d6bd3ae565ec09bddd9c9066553698841bb
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-04-11 15:06:11 +0300

    get back chef-mode

--- scripts/chef.sh
+++ scripts/chef.sh
@@ -2,7 +2,7 @@
 
 . config.in
 
-# CHEF_MODE_URL="https://raw.githubusercontent.com/mpasternacki/chef-mode/master/chef-mode.el"
+CHEF_MODE_URL="https://raw.githubusercontent.com/mpasternacki/chef-mode/master/chef-mode.el"
 CHEF_URL="https://raw.githubusercontent.com/cosmonaut-ok/emacs-chef/master/chef.el"
 KITCHEN_URL="https://raw.githubusercontent.com/restaurant-ide/test-kitchen-el/master/test-kitchen.el"
 KNIFE_URL="https://raw.githubusercontent.com/bryanwb/knife-mode/master/knife.el"
@@ -11,13 +11,13 @@ FOODCRITIC_URL="https://raw.githubusercontent.com/restaurant-ide/foodcritic.el/m
 function pkg_install
 {
     get_url_with_name chef.el $CHEF_URL
-    # get_url_with_name chef-mode.el $CHEF_MODE_URL
+    get_url_with_name chef-mode.el $CHEF_MODE_URL
     get_url_with_name test-kitchen.el $KITCHEN_URL
     get_url_with_name knife.el $KNIFE_URL
     get_url_with_name berkshelf.el $BERKS_URL
     get_url_with_name foodcritic.el $FOODCRITIC_URL
     copy_to_local chef.el chef
-    # copy_to_local chef-mode.el chef
+    copy_to_local chef-mode.el chef
     copy_to_local test-kitchen.el chef
     copy_to_local knife.el chef
     copy_to_local berkshelf.el chef
