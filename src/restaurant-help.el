46c2620bad4f9d44a11267105d8dd681d3c62985
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-04-12 18:37:47 +0300

    Add default restaurant.conf for delivery

--- src/restaurant-help.el
+++ src/restaurant-help.el
@@ -29,7 +29,7 @@
 
 (defun restaurant-version (&optional short)
   (interactive)
-  (let ((version "0.3.4")) ;; autoconf-anchor
+  (let ((version "0.3.5")) ;; autoconf-anchor
     (if short
 	(princ version)
       (princ (concat "Restaurant Chef IDE version: " version ".\nBuild with Emacs version:\n" (emacs-version) "\n")))))
